258555893dec9870bce8ba4085e23555b2d1ac81
Werbrouck Dylan <dylan.werbrouck@hotmail.com>
2020-03-02 10:04:58 +0100

    upate keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Dolan:VvrLvys3oZ8JetkO@testcluster-eeu2b.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Dolan:HAHAPASWOORDHIERHAHAH@testcluster-eeu2b.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
